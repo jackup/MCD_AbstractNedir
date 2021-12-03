90970015cff0c0aa4eacc7735b1ac0851f96568b
jackup <yk.58@hotmail.com>
2021-12-03 17:38:32 +0300

    Add project files.

---
+++ MCD_AbstractNedir/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_AbstractNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abstract ve Interface Kavramları
+            //
+            //Kendimize işi yapacak bir rol model çıkartırız. Nasıl olacağının kurallarını hazırlar ve nesnelerimizde kurallarımızı kullanırız.
+            //Bu bölümde oluşturmuş olduğumuz nesnelerimizi nasıl kurallar ile şartlayabileceğimizi konuşacağız.
+            //
+        }
+    }
+}
